1e4e074fc4593e8668c1b92088a83b507b9ce50e
devWRM <mcwen@comcast.net>
2020-06-16 17:02:59 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(names)
+    names.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+
+def assign_rooms(names)
+    speaker_rooms = []
+    names.each_with_index do |name, index|
+       speaker_rooms << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+    speaker_rooms
+end
+
+
+def printer(names)
+    batch_badge_creator(names).each {|badge| puts "#{badge}"}
+    assign_rooms(names).each {|assignment| puts "#{assignment}"}
+end
+
+
+
+
