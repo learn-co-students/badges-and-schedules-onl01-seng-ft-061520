e97dfaaffde8c85b509ec930c26cea891ab02bf0
Adam Workman <adammworkman25@gmail.com>
2020-06-17 14:51:37 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each |attendee|
+  attendees.collect do |attendee|
+    "Hello, my name is #{attendee}."
+  end
 end
 
 def assign_rooms
