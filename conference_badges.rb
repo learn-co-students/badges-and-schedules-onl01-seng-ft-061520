9db46689d641db87b627e0693c35f61aefd8ede3
Adam Workman <adammworkman25@gmail.com>
2020-06-17 15:08:45 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,9 @@ def batch_badge_creator(attendees)
   end
 end
 
-def assign_rooms
+def assign_rooms(attendees)
+  attendees.collect do |attendee| "Hello, #{attendee}! You'll be assigned to room #{attendee.index}!"
+end
 end
 
 def printer
