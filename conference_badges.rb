a0d65f986175e8511e8a650b53351468a667da69
Devon Wehrlin <dkwehrlin.99@gmail.com>
2020-06-17 20:49:36 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,37 +4,25 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  badges = []
-  attendees.each do |badge|
-    puts "Hello, my name is #{badge}."
-  badges << "Hello, my name is #{badge}."
+  attendees.collect do |name|
+    badge_maker(name)
   end
-  return badges
+
 end
 
 def assign_rooms(attendees)
-  rooms = []
-  attendees.each_with_index do |speaker, index|
-    index += 1
-    puts "Hello, #{speaker}! You'll be assigned to room #{index}!"
-  rooms << "Hello, #{speaker}! You'll be assigned to room #{index}!"
+  room=0
+  attendees.collect do |name|
+    room+=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
   end
-  return rooms
 end
 
-#def printer(attendees)
- # badges_and_room_assignments.each_line do |line|
- #   puts line 
-#  end
-  
-#end
-
 def printer(attendees)
-  batch_badge_creator(attendees).each do |badge|
-    puts badge
+  batch_badge_creator(attendees).each do |value|
+    puts value
   end
-
-  assign_rooms(attendees).each do |assignment|
-    puts assignment
+  assign_rooms(attendees).each do |value|
+    puts value
   end
 end
