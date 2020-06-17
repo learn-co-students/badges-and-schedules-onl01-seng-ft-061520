f960384cbf7a97dc7cb7b56ee30bbab008698839
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-17 18:52:47 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator[]
+  new_array = []
+    array.each do |name|
+  new_array << ("Hello, my name is #{name}.")
