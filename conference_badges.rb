dbb304ef137cac28ee636ccc073afa5328e2f1bc
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-17 19:24:56 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,42 @@
+# 'should return a formatted badge'
+
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator[]
+# 'should return a list of badge messages'
+
+def batch_badge_creator(array)
   new_array = []
     array.each do |name|
   new_array << ("Hello, my name is #{name}.")
+end
+    return new_array
+end
+
+# 'should return a list of welcome messages and room assignments'
+
+def assign_rooms(array)
+  new_array = []
+  counter = 1
+    array.each do |name|
+    new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+  counter += 1
+end
+  return new_array
+end
+
+# 'printer'
+
+def printer(array)
+  batch_badge_creator(array).each do |individual|
+    puts individual 
+  end
+
+# 'assign rooms'
+
+assign_rooms(array).each do |individual|
+    puts individual
+  end
+end
+  
